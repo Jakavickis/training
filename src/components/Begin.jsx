20533ac5cce62860a6d9fcf9e4393889a04ec7ad
Jakavickis <vytautasjkvcks@gmail.com>
2022-10-02 21:10:30 +0300

    ?

--- src/components/Begin.jsx
+++ src/components/Begin.jsx
@@ -1,4 +1,18 @@
+import axios from 'axios'
+import { useEffect, useState } from 'react';
+
 function Begin() {
+
+    const [owners, setOwners] = useState(null);
+
+    useEffect(() => {
+        axios.get('http://localhost:3003/api')
+            .then(res => {
+                console.log(res.data)
+                setOwners(res.data);
+            });
+    }, []);
+
     return (
         <>
             <div>hi world</div>
